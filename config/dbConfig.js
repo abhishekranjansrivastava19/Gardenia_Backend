f6258a282beffffa9905845ab56deedae9d3b2d2
unknown <abhishekranjansrivastava19@gmail.com>
2025-02-03 12:59:02 +0530

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,9 +1,22 @@
-module.exports = {
+// module.exports = {
+//     // Metadata database configuration
+//     metadataDBConfig: {
+//       //    user: 'LAPTOP-JO66B6L3\\ABHISHEK', // Your database username
+//       //    password: '', // Your database password
+//       server: "LAPTOP-JO66B6L3\\SQLEXPRESS", // Your database server IP address
+//       database: "GardeniaRegistrationPortal", // Your database name
+//       options: {
+//         trustedConnection: true,
+//       },
+//     },
+//   };
+
+  module.exports = {
     // Metadata database configuration
     metadataDBConfig: {
-      //    user: 'LAPTOP-JO66B6L3\\ABHISHEK', // Your database username
-      //    password: '', // Your database password
-      server: "LAPTOP-JO66B6L3\\SQLEXPRESS", // Your database server IP address
+      user: 'dpsuser', // Your database username
+      password: 'dps@123', // Your database password
+      server: "250.242.203.229", // Your database server IP address
       database: "GardeniaRegistrationPortal", // Your database name
       options: {
         trustedConnection: true,
